6082f0d28686fd484bab387738a4b44e64eb28e0
wuzonghui <310541691@qq.com>
2018-01-05 10:04:52 +0800

    提交小汽车

--- ssmo/src/main/java/com/ssmo/pojo/Car.java
+++ ssmo/src/main/java/com/ssmo/pojo/Car.java
@@ -1,4 +1,18 @@
 package com.ssmo.pojo;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.util.Locale;
+import java.util.stream.IntStream;
+
 public class Car {
+    private  String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
